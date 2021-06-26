116cba9efcb460e7f50be446e2a9edf95f79b4ec
Timtohy Eiffe <kaotech@kat.fritz.box>
2021-06-26 21:45:25 +0930

    added ft_putchar

--- Rush00/rush02.c
+++ Rush00/rush02.c
@@ -22,24 +22,26 @@ void	rush(int x, int y)
 			while (i <= x)
 				if (i == 1 && i == x)
 				{
-					write(1, "A\n", 2);
+					ft_putchar('A');
+					ft_putchar('\n');
 					l++;
 					break;
 				}
 				else if (i == 1)
 				{
-					write(1, "A", 1);
+					ft_putchar('A');
 					i++;
 				}
 				else if (i == x)
 				{
-					write(1, "A\n", 2);
+					ft_putchar('A');
+					ft_putchar('\n');
 					l++;
 					break;
 				}
 				else
 				{
-					write(1, "B", 1);
+					ft_putchar('B');
 					i++;
 				}
 		}
@@ -49,24 +51,26 @@ void	rush(int x, int y)
 			while (j <= x)
 				if (j == 1 && j == x)
 				{
-					write(1, "C\n", 2);
+					ft_putchar('C');
+					ft_putchar('\n');
 					l++;
 					break;
 				}
 				else if (j == 1)
 				{
-					write(1, "C", 1);
+					ft_putchar('C');
 					j++;
 				}
 				else if (j == x)
 				{
-					write(1, "C\n", 2);
+					ft_putchar('C');
+					ft_putchar('\n');
 					l++;
 					break;
 				}
 				else
 				{
-					write(1, "B", 1);
+					ft_putchar('B');
 					j++;
 				}
 			
@@ -77,24 +81,26 @@ void	rush(int x, int y)
 			while (k <= x)
 				if (k == 1 && k == x)
 				{
-					write(1, "B\n", 2);
+					ft_putchar('B');
+					ft_putchar('\n');
 					l++;
 					break;
 				}
 				else if (k == 1)
 				{
-					write(1, "B", 1);
+					ft_putchar('B');
 					k++;
 				}
 				else if (k == x)
 				{
-					write(1, "B\n", 2);
+					ft_putchar('B');
+					ft_putchar('\n');
 					l++;
 					break;
 				}
 				else
 				{
-					write(1, " ", 1);
+					ft_putchar(' ');
 					k++;
 				}
 		}
@@ -105,6 +111,6 @@ void	rush(int x, int y)
 /*ENDS HERE*/
 int		main(void)
 {
-	rush(4, 4);
+	rush(5, 1);
 	return (0);
 }
