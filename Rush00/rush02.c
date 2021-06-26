7663896f668793b6b6cd301c05a9bad934a0e90d
Timtohy Eiffe <kaotech@kat.fritz.box>
2021-06-26 20:20:14 +0930

    first commit

---
+++ Rush00/rush02.c
@@ -0,0 +1,52 @@
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	print_horizontal(int x, char left, char middle, char right)
+{
+	if (x-- >= 1)
+	{
+		ft_putchar(left);
+		while (x-- >= 2)
+		{
+			ft_putchar(middle);
+		}
+		if (x == 0)
+			ft_putchar(right);
+			ft_putchar('\n');
+	}
+}
+
+void	print_vertical(int x, int y, char wall)
+{
+	int i;
+
+	while (y-- > 2)
+	{
+		ft_putchar(wall);
+		i = 2;
+		while (x > i++)
+			ft_putchar(' ');
+		ft_putchar(wall);
+		ft_putchar('\n');
+	}
+}
+
+void	rush(int x, int y)
+{
+	if (y >= 1)
+		print_horizontal(x, 'A', 'B', 'A');
+	print_vertical(x, y, 'B');
+	if (y >= 2)
+		print_horizontal(x, 'C', 'B', 'C');
+}
+
+int             main()
+{
+        rush(1, 5);
+        return (0);
+}
+
