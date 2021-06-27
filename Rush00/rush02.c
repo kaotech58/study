71e04f08af3c294414862391d24c04209d12d253
Timtohy Eiffe <kaotech@kat.fritz.box>
2021-06-27 10:57:50 +0930

    cleaned up code

--- Rush00/rush02.c
+++ Rush00/rush02.c
@@ -4,113 +4,68 @@ void	ft_putchar(char c)
 {
 	write(1, &c, 1);
 }
-/*STARTS HERE */
 
-void	rush(int x, int y)
+void rows(int x, char start, char middle, char end)
 {
-	int i;
-	int j;
-	int k;
-	int l;
+	int	count;
 
-	l = 1;
-	while (l <= y)
+	count = 1;
+	while (count <= x)
 	{
-		if (l == 1)
+		if (count == 1 && count == x)
 		{
-			i = 1;
-			while (i <= x)
-				if (i == 1 && i == x)
-				{
-					ft_putchar('A');
-					ft_putchar('\n');
-					l++;
-					break;
-				}
-				else if (i == 1)
-				{
-					ft_putchar('A');
-					i++;
-				}
-				else if (i == x)
-				{
-					ft_putchar('A');
-					ft_putchar('\n');
-					l++;
-					break;
-				}
-				else
-				{
-					ft_putchar('B');
-					i++;
-				}
+			ft_putchar(start);
+			ft_putchar('\n');
+			break;
 		}
-		else if (l == y)
+		else if (count == 1)
 		{
-			j = 1;
-			while (j <= x)
-				if (j == 1 && j == x)
-				{
-					ft_putchar('C');
-					ft_putchar('\n');
-					l++;
-					break;
-				}
-				else if (j == 1)
-				{
-					ft_putchar('C');
-					j++;
-				}
-				else if (j == x)
-				{
-					ft_putchar('C');
-					ft_putchar('\n');
-					l++;
-					break;
-				}
-				else
-				{
-					ft_putchar('B');
-					j++;
-				}
-			
+			ft_putchar(start);
+			count++;
+		}
+		else if (count == x)
+		{
+			ft_putchar(end);
+			ft_putchar('\n');
+			break;
 		}
 		else
 		{
-			k = 1;
-			while (k <= x)
-				if (k == 1 && k == x)
-				{
-					ft_putchar('B');
-					ft_putchar('\n');
-					l++;
-					break;
-				}
-				else if (k == 1)
-				{
-					ft_putchar('B');
-					k++;
-				}
-				else if (k == x)
-				{
-					ft_putchar('B');
-					ft_putchar('\n');
-					l++;
-					break;
-				}
-				else
-				{
-					ft_putchar(' ');
-					k++;
-				}
+			ft_putchar(middle);
+			count++;
+		}
+	}
+
+}
+
+void	rush(int x, int y)
+{
+	int	count;
+
+	count = 1;
+	while (count <= y)
+	{
+		if (count == 1)
+		{
+			rows(x, 'A', 'B', 'A');
+			count++;
+		}
+		else if (count == y)
+		{
+			rows(x, 'C', 'B', 'C');
+			count++;
+		}
+		else
+		{
+			rows(x, 'B', ' ', 'B');
+			count++;
 		}
 
 	}
 }
 
-/*ENDS HERE*/
 int		main(void)
 {
-	rush(5, 1);
+	rush(25, 15);
 	return (0);
 }
