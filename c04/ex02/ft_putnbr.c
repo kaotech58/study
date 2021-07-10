15392ae48ae17e3039fceff255d9fe0df0332e4d
Timtohy Eiffe <kaotech@kat.fritz.box>
2021-07-10 13:53:40 +0930

    first commit

---
+++ c04/ex02/ft_putnbr.c
@@ -0,0 +1,39 @@
+#include <unistd.h>
+
+void	ft_putchar(char c);
+
+void	ft_putnbr(int nb)
+{
+	long	nbr;
+
+	nbr = nb;
+	if (nbr < 0)
+	{
+		ft_putchar('-');
+		nbr = -nbr;
+	}
+	if (nbr >= 10)
+	{
+		ft_putnbr(nbr / 10);
+		ft_putnbr(nbr % 10);
+	}
+	else
+	{
+		ft_putchar(nbr + '0');
+	}
+}
+
+void ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+int	main(void)
+{
+	ft_putnbr(-2147483648);
+	ft_putchar('\n');
+	ft_putnbr(42);
+	ft_putchar('\n');
+	ft_putnbr(-2147473647);
+	ft_putchar('\n');
+}
